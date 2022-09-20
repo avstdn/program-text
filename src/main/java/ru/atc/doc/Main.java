a79f5ed909c5ab9f7f02207f1ed6839563c5f314
astudenikin <astudenikin@phoenixit.ru>
2022-09-21 01:41:15 +0300

    (feat) Добавил дополнительные параметры: расширения файлов и игнорируемые директории. Убрал печать названия пакета

--- src/main/java/ru/atc/doc/Main.java
+++ src/main/java/ru/atc/doc/Main.java
@@ -5,13 +5,13 @@ public class Main {
 	public static void main(String[] args) {
 		if (arePresent(args)) {
 			ProgramText programText = new ProgramText();
-			programText.generate(args[0]);
+			programText.generate(args[0], args[1], args[2]);
 		}
 	}
 
 	private static boolean arePresent(String[] args) {
-		if (args.length < 1) {
-			System.out.println("Path to project source code folder not specified in command line args");
+		if (args.length < 3) {
+			System.out.println("Path to project or extensions or ignore folders are not specified in command line args");
 			return false;
 		}
 		return true;
--- src/main/java/ru/atc/doc/ProgramText.java
+++ src/main/java/ru/atc/doc/ProgramText.java
@@ -8,17 +8,14 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
-import java.util.regex.Pattern;
 
 public class ProgramText {
 
-	public void generate(String sourceFolderPath) {
+	public void generate(String sourceFolderPath, String extensions, String ignoreFolders) {
 		try {
 			PrintWriter printWriter = new PrintWriter(getOutputFileName());
-			writeOutputFile(sourceFolderPath, printWriter);
+			writeOutputFile(sourceFolderPath, printWriter, extensions, ignoreFolders);
 			printWriter.flush();
 			System.out.println("SUCCESS");
 		} catch (IOException e) {
@@ -32,30 +29,35 @@ public class ProgramText {
 		return String.format("output-%s.txt", LocalDateTime.now().format(dateTimeFormatter));
 	}
 
-	private void writeOutputFile(String sourceFolderPath, PrintWriter printWriter) throws IOException {
+	private void writeOutputFile(String sourceFolderPath, PrintWriter printWriter, String extensions, String ignoreFolders) throws IOException {
 		File rootFolder = new File(sourceFolderPath);
 		String[] fileNames = rootFolder.list();
+		String[] ignoreFoldersArray = ignoreFolders.split(",");
 
 		if (fileNames != null && fileNames.length > 0) {
-			String[] javaFiles = getJavaFiles(rootFolder);
+			String[] files = getFiles(rootFolder, extensions);
 
-			if (javaFiles != null && javaFiles.length > 0) {
-				writePackageName(printWriter, rootFolder, javaFiles[0]);
-				writeJavaFiles(printWriter, rootFolder, javaFiles);
+			if (files != null && files.length > 0) {
+//				writePackageName(printWriter, rootFolder, files[0], startPackage);
+				writeFiles(printWriter, rootFolder, files);
 			}
 
 			for (String fileName : fileNames) {
 				File f = new File(rootFolder, fileName);
 
 				if (f.isDirectory()) {
-					System.out.println("Reading directory: " + f.getName());
-					writeOutputFile(f.getAbsolutePath(), printWriter);
+					if (Arrays.stream(ignoreFoldersArray).noneMatch(f.getAbsolutePath()::equalsIgnoreCase)) {
+						System.out.println("+ Reading directory: " + f.getName());
+						writeOutputFile(f.getAbsolutePath(), printWriter, extensions, ignoreFolders);
+					} else {
+						System.out.println("- Ignore directory: " + f.getName());
+					}
 				}
 			}
 		}
 	}
 
-	private void writeJavaFiles(PrintWriter printWriter, File rootFolder, String[] javaFiles) throws IOException {
+	private void writeFiles(PrintWriter printWriter, File rootFolder, String[] javaFiles) throws IOException {
 		for (String fileName : javaFiles) {
 			System.out.println("Reading file: " + fileName);
 			File f = new File(rootFolder, fileName);
@@ -73,24 +75,25 @@ public class ProgramText {
 		}
 	}
 
-	private String[] getJavaFiles(File rootFolder) {
-		FilenameFilter javaExtension = (f, name) -> name.endsWith(".java");
-		return rootFolder.list(javaExtension);
+	private String[] getFiles(File rootFolder, String extensionString) {
+		String[] extensions = extensionString.split(",");
+		FilenameFilter filteredExtensions = (f, name) -> Arrays.stream(extensions).anyMatch(name::endsWith);
+		return rootFolder.list(filteredExtensions);
 	}
 
-	private void writePackageName(PrintWriter pw, File rootFolder, String javaFile) {
-		String pathString = new File(rootFolder, javaFile).getPath();
-		String fileSeparator = Pattern.quote(System.getProperty("file.separator"));
-		String[] splitPath = pathString.split(fileSeparator);
-		int startPackageWordIndex = Arrays.asList(splitPath).indexOf("ru");
-		List<String> list = new ArrayList<>();
-
-		for (int i = startPackageWordIndex; i < splitPath.length - 1; i++) {
-			list.add(splitPath[i]);
-		}
-
-		String packageString = String.join(".", list);
-		pw.println("Пакет " + packageString);
-		pw.println();
-	}
+//	private void writePackageName(PrintWriter pw, File rootFolder, String javaFile, String startPackage) {
+//		String pathString = new File(rootFolder, javaFile).getPath();
+//		String fileSeparator = Pattern.quote(System.getProperty("file.separator"));
+//		String[] splitPath = pathString.split(fileSeparator);
+//		int startPackageWordIndex = Arrays.asList(splitPath).indexOf(startPackage);
+//		List<String> list = new ArrayList<>();
+//
+//		for (int i = startPackageWordIndex; i < splitPath.length - 1; i++) {
+//			list.add(splitPath[i]);
+//		}
+//
+//		String packageString = String.join(".", list);
+//		pw.println("Пакет " + packageString);
+//		pw.println();
+//	}
 }
